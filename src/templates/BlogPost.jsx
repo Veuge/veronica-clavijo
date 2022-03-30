0dc95ff9258c370ce9bbf07432dce68d04e55bb8
Veronica Clavijo <veuge.clavijo@gmail.com>
2022-03-30 16:27:40 -0400

    Blog post pages (#10)
    * Code hightlighting working with MDX
    
    * Blog post for mobile.
    
    * Styling blogpost - only mobile.
    
    * Rename pages to .mdx extension.
    
    * Responsiveness desktop and mobile.

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -2,18 +2,31 @@ import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
 import { Tag } from "../components/Tag";
+import CodeBlock from "../components/Code";
+import { MDXProvider } from "@mdx-js/react";
+import * as styles from "../styles/blogpost.module.scss";
 
-const BlogPost = (props) => {
-  console.log({props, data: props.data, title: props.data.mdx})
+const components = {
+  pre: CodeBlock
+}
+
+const BlogPost = ({ data }) => {
+  const { mdx: { frontmatter, body } } = data;
   return (
-    <div>
-      <h1>{props.data.mdx.frontmatter.title}</h1>
-      <h2>{props.data.mdx.frontmatter.date}</h2>
-      {!!props.data.mdx.frontmatter.tags && props.data.mdx.frontmatter.tags.map(tag => (
-        <Tag key={tag} tag={tag} />
-      ))}
-      <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
-    </div>
+    <article className={styles.blogPostContainer}>
+      <div className={styles.post}>
+        <h1 className={styles.postTitle}>{frontmatter.title}</h1>
+        <div className={styles.metaInfoContainer}>
+          <span>{frontmatter.date}</span>
+          {!!frontmatter.tags && frontmatter.tags.map(tag => (
+            <Tag key={tag} tag={tag} />
+          ))}
+        </div>
+        <MDXProvider components={components}>
+          <MDXRenderer>{body}</MDXRenderer>
+        </MDXProvider>
+      </div>
+    </article>
   );
 };
 
