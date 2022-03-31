d2639cd487b218164da7b4670236d7e58075945c
Veronica Clavijo <veuge.clavijo@gmail.com>
2022-03-30 20:17:01 -0400

    Fixing images in mdx files. (#12)

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import { Tag } from "../components/Tag";
 import CodeBlock from "../components/Code";
 import { MDXProvider } from "@mdx-js/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+
 import * as styles from "../styles/blogpost.module.scss";
 
 const components = {
@@ -18,9 +21,17 @@ const BlogPost = ({ data }) => {
         <h1 className={styles.postTitle}>{frontmatter.title}</h1>
         <div className={styles.metaInfoContainer}>
           <span>{frontmatter.date}</span>
-          {!!frontmatter.tags && frontmatter.tags.map(tag => (
-            <Tag key={tag} tag={tag} />
-          ))}
+          {!!frontmatter.tags.length && (
+            <>
+              <FontAwesomeIcon
+                icon={faEllipsisV}
+                className={styles.separatorIcon}
+              />
+              {frontmatter.tags.map(tag => (
+                <Tag key={tag} tag={tag} />
+              ))}
+            </>
+          )}
         </div>
         <MDXProvider components={components}>
           <MDXRenderer>{body}</MDXRenderer>
