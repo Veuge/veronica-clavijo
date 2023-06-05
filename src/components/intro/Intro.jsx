e4a01f60200c8c8c95b910ecddd11c0c7c80293a
Veronica Clavijo <veuge.clavijo@gmail.com>
2023-06-04 23:53:49 -0400

    6 whoami (#28)
    * Reuse Presentation component for whoami section.
    
    * 6. Who am i section completed.
    
    * 6 update presentation.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -13,16 +13,35 @@ const Intro = ({ file, links }) => {
         className={styles.avatar}
       />
       <div className={styles.description}>
-        <p>Hola! soy Verónica Clavijo y este es mi <a href="https://maggieappleton.com/garden-history" target="__blank">jardín digital</a> lo que quiere decir que en este blog nada es definitivo, todo está sujeto a cambio, o sea la información puede cambiar, crecer o evolucionar.</p>
-        <p>Escribo principalmente de JS, React, React Native y mi camino hacia convertirme es FullStack Developer.</p>
-        <p>Si quieres charlar mandame un <a className={styles.redc} href={`mailto:${links.email}`}>email</a>
-          , escribeme un <a className={styles.greenc} href={links.twitter} target="__blank">tweet</a> o un <a className={styles.bluec} href={links.linkedin} target="__blank">
+        <p>
+          Hola! soy Verónica Clavijo y este es mi{" "}
+          <a href="https://maggieappleton.com/garden-history" target="__blank">
+            jardín digital
+          </a>{" "}
+          lo que quiere decir que en este blog nada es definitivo, todo está sujeto a cambio, o sea
+          la información puede cambiar, crecer o evolucionar.
+        </p>
+        <p>
+          Escribo principalmente de JS, React, React Native y mi camino hacia convertirme es
+          FullStack Developer.
+        </p>
+        <p>
+          Si quieres charlar mandame un{" "}
+          <a className={styles.redc} href={`mailto:${links.email}`}>
+            email
+          </a>
+          , escribeme un{" "}
+          <a className={styles.greenc} href={links.twitter} target="__blank">
+            tweet
+          </a>{" "}
+          o un{" "}
+          <a className={styles.bluec} href={links.linkedin} target="__blank">
             mensaje directo
           </a>
         </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Intro;
