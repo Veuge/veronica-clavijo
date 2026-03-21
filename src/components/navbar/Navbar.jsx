92d33b4f1bdcf56688dfd1584246223ce708f3d4
Veronica Clavijo <veuge.clavijo@gmail.com>
2026-03-20 22:05:59 -0400

    39: Translating navbar and home page.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
 import { composeClasses } from "../../helpers/index";
 import Header from "./Header";
@@ -7,6 +8,7 @@ import * as styles from "./navbar.module.scss";
 import LanguageSwitcher from "../languageSwitcher/LanguageSwitcher";
 
 export default function Navbar() {
+  const { t } = useTranslation();
   return (
     <nav className={styles.navbar}>
       <Header />
@@ -16,7 +18,7 @@ export default function Navbar() {
           to="/posts"
           activeClassName={styles.activeNavLink}
         >
-          Anotaciones
+          {t('blog_section')}
         </Link>
         {/* <Link
           className={composeClasses(styles.navLink, styles.navLinkRed)}
